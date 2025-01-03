5da26bc48e6fa3fad1a672d70699e0eecbd48e83
Alan Bach <kagamibach@gmail.com>
2025-01-03 16:36:56 -0700

    Easily readable

--- src/Classes/Formatters/default_formatter.py
+++ src/Classes/Formatters/default_formatter.py
@@ -7,7 +7,11 @@ class DefaultFormatter(Formatter):
     MODEL_NAME = "Default"
     MODEL_LABEL = "assistant"
     USER_LABEL = "user"
-    CHAT_TEMPLATE = "{% if not add_generation_prompt is defined %}{% set add_generation_prompt = false %}{% endif %}{% for message in messages %}{{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}{% endfor %}{% if add_generation_prompt %}{{ '<|im_start|>assistant\n' }}{% endif %}"
+    CHAT_TEMPLATE = (
+        "{% if not add_generation_prompt is defined %}{% set add_generation_prompt = false %}{% endif %}"
+        "{% for message in messages %}{{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}{% endfor %}"
+        "{% if add_generation_prompt %}{{ '<|im_start|>assistant\n' }}{% endif %}"
+    )
 
     @override
     def __init__(self, tokenizer):
