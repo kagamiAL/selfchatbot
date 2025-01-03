288a439d464fd13bad6f645f263249a6f3865592
Alan Bach <kagamibach@gmail.com>
2025-01-03 15:36:36 -0700

    Just found out tokenizers have an apply chat template method

---
+++ src/Classes/Formatters/default_formatter.py
@@ -0,0 +1,24 @@
+from Classes.Formatters.formatter import Formatter
+from Classes.TypeDicts import MessagePacket
+from typing import override
+
+
+class DefaultFormatter(Formatter):
+    MODEL_NAME = "Default"
+    MODEL_LABEL = "assistant"
+    USER_LABEL = "user"
+    CHAT_TEMPLATE = "{% if not add_generation_prompt is defined %}{% set add_generation_prompt = false %}{% endif %}{% for message in messages %}{{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}{% endfor %}{% if add_generation_prompt %}{{ '<|im_start|>assistant\n' }}{% endif %}"
+
+    @override
+    def __init__(self, tokenizer):
+        super().__init__(tokenizer)
+        if not self.tokenizer.chat_template:
+            self.tokenizer.chat_template = self.CHAT_TEMPLATE
+
+    @override
+    def format_block(
+        self, block: list[MessagePacket], add_generation_prompt: bool = False
+    ) -> str:
+        return self.tokenizer.apply_chat_template(
+            block, tokenize=False, add_generation_prompt=add_generation_prompt
+        )
